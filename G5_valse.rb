f870d164910fad783353a6b4f91ad878b2f9bfe2
Dmitri Surchis <dsurchis@tacitknowledge.com>
2020-08-29 17:46:38 +0300

    add first track - valse in G5

---
+++ G5_valse.rb
@@ -0,0 +1,48 @@
+$temp = 0.3
+$direct = 0.3
+
+
+live_loop :bou do
+  play :G5
+  sleep $temp
+  play :Fb5
+  sleep $temp
+  play :C6
+  sleep $temp
+end
+
+live_loop :bass do
+  play :G2
+  sleep $temp * 3
+end
+
+live_loop :china do
+  sleep $temp
+  sample :guit_e_slide, rate: 0.66, slice: 1
+  sleep $temp * rrand_i(2,3)
+end
+
+live_loop :amb do
+  sample :ambi_drone
+  sync :bass
+end
+
+
+live_loop :hat do
+  sample :drum_cymbal_soft
+  sleep $direct * rrand_i(1,4) /2
+end
+
+live_loop :dir do
+  sample :drum_snare_soft
+  sleep $direct * 4
+end
+
+live_loop :roll do
+  loop do
+    sample :drum_bass_hard
+    sleep $direct/2
+  end
+  sleep $direct/2
+end
+
