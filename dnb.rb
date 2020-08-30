9fb76ff11b9d3051d76be48dd4e6a7892a036708
Dmitri Surchis <dsurchis@tacitknowledge.com>
2020-08-30 17:22:36 +0300

    randomize dnb, use play instead of samples

--- dnb.rb
+++ dnb.rb
@@ -11,46 +11,18 @@ live_loop :drums do
   sleep $phase
 end
 
-live_loop :bass do
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 4, pan: -1
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 3, pan: -0.5
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 2, pan: 0.5
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 1, pan: 1
+live_loop :random_bass do
+  play choose [:C2, :D2, :E2]
   
-  sleep $eighth
-  sample :bass_thick_c, rate: 0.7, slice: $phase * 4, pan: -1
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 3, pan: -0.5
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 2, pan: 0.5
-  sample :bass_thick_c, rate: 1, slice: $phase * 1, pan: 1
-  
-  sleep $eighth/2
-  sample :bass_thick_c, rate: 1, slice: $phase * 4, pan: -1
-  sleep $eighth
-  sample :bass_thick_c, rate: 1, slice: $phase * 3, pan: -0.5
-  sleep $eighth/2
-  sample :bass_thick_c, rate: 1, slice: $phase * 2, pan: 0.5
-  sample :bass_thick_c, rate: 1, slice: $phase * 1, pan: 1
-  
-  sleep $eighth/2
-  sample :bass_thick_c, rate: 0.7, slice: $phase * 4, pan: -1
-  sleep $eighth
-  sample :bass_thick_c, rate: 0.7, slice: $phase * 3, pan: -0.5
-  sample :bass_thick_c, rate: 0.7, slice: $phase * 2, pan: 0.5
-  sleep $eighth
-  sample :bass_thick_c, rate: 0.7, slice: $phase * 1, pan: 1
-  
-  sleep $half
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 8, pan: -1
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 3, pan: -0.5
-  sleep $eighth/2
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 2, pan: 0.5
-  sample :bass_thick_c, rate: 0.8, slice: $phase * 1, pan: 1
-  
-  sync :drums
+  sleep $quater
 end
 
 live_loop :ambient do
-  sample :ambi_glass_hum
-  sync :drums
+  sample :ambi_lunar_land
+  sleep $phase * 4
 end
 
+live_loop :hat do
+  sample :drum_cymbal_soft, pan: choose([-1, 1])
+  sleep choose [$quater, $half, $eighth]
+end
